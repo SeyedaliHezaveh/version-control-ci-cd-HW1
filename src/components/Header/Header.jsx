21284f5c07b75fd8c3c19a4ff939a1c4cc077d47
Mohammad Rezaie <mrezaie264@gmail.com>
2024-10-18 12:18:24 +0330

    add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../../logo.svg'
 
 const Header = () => {
   return (
     <header>
+     <img src={logo} alt="Logo" style={{ height: '50px' }} />
       <h1>My Static Site</h1>
       <nav>
         <ul>
