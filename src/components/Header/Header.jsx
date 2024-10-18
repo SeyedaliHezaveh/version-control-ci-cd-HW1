6b2dd121e0f6587d151e5a891b8e1bdfb995e449
Mohammad Rezaie <mrezaie264@gmail.com>
2024-10-18 12:22:18 +0330

    add button to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import logo from '../../logo.svg'
+import React from "react";
+import logo from "../../logo.svg";
 
 const Header = () => {
   return (
     <header>
-     <img src={logo} alt="Logo" style={{ height: '50px' }} />
+      <img src={logo} alt="Logo" style={{ height: "50px" }} />
       <h1>My Static Site</h1>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li>
+            <button>Home</button>
+          </li>
+          <li>
+            <button>About</button>
+          </li>
+          <li>
+            <button>Contact</button>
+          </li>
         </ul>
       </nav>
     </header>
-
   );
 };
 
