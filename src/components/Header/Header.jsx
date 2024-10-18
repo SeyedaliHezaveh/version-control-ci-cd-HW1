0d0f4dbff9f79261fadcfe66b48cca94ca66d1f3
Mohammad Rezaie <mrezaie264@gmail.com>
2024-10-18 12:15:52 +0330

    add basic structure to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,15 @@ const Header = () => {
   return (
     <header>
       <h1>My Static Site</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
+
   );
 };
 
